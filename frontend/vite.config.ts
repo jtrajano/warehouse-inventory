ec54cda46042c43e2a21260a024531647a3c4bdb
jtrajano <jlt212000@yahoo.com>
2026-06-19 21:48:10 +0800

    closes Github #2

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, './src'),
+      '@components':path.resolve(__dirname,'./src/components'),
+      '@lib':path.resolve(__dirname,'./src/lib'),
+      '@hooks':path.resolve(__dirname,'./src/hooks'),
+      '@mocks':path.resolve(__dirname,'./src/mocks'),
+      '@pages':path.resolve(__dirname,'./src/pages'),
+      '@types':path.resolve(__dirname,'./src/types'),
+      '@assets':path.resolve(__dirname,'./src/assets')
+    }
+  },
+  plugins: [react()],
+})
